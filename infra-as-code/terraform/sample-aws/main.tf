79ec2608bfe1e13c87c742a4b09d294618ccd66b
goyaldivij-egov <divij.goyal@equidhi.org>
2025-02-03 13:22:27 +0530

    Added kubernetes cluster tags for limited iam access

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -142,18 +142,33 @@ resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = var.cluster_name
   addon_name        = "kube-proxy"
   resolve_conflicts = "OVERWRITE"
+  tags = {
+    Environment = var.cluster_name
+    Terraform   = "true"
+    "KubernetesCluster" = var.cluster_name
+  }
 }
 resource "aws_eks_addon" "core_dns" {
   depends_on = [module.eks_managed_node_group]
   cluster_name      = var.cluster_name
   addon_name        = "coredns"
   resolve_conflicts = "OVERWRITE"
+  tags = {
+    Environment = var.cluster_name
+    Terraform   = "true"
+    "KubernetesCluster" = var.cluster_name
+  }
 }
 resource "aws_eks_addon" "aws_ebs_csi_driver" {
   depends_on = [module.eks_managed_node_group]
   cluster_name      = var.cluster_name
   addon_name        = "aws-ebs-csi-driver"
   resolve_conflicts = "OVERWRITE"
+  tags = {
+    Environment = var.cluster_name
+    Terraform   = "true"
+    "KubernetesCluster" = var.cluster_name
+  }
 }
 
 provider "kubernetes" {
